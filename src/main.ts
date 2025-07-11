628ef8d4c5a5f1ce324b5244b934b7160cf81d63
Cesar Suazo <suazoromerojamil@gmail.com>
2025-07-10 22:51:31 -0500

    Agregando la IP de mi maquina

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
     origin: [
       'http://192.168.18.11:3000',
       'http://192.168.18.11:8080',
+      'http://192.168.1.44:8080',
+      //Colocar o añadir la IP de tu maquina 
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
